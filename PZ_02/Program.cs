c66f6cb9c5f0db1eaad4e50791340e8b50fb5a28
Иван Бабан <zxc228deadinside@yandex.ru>
2023-09-17 17:31:36 +0500

    PZ_01, PZ_02, PZ_03 update

---
+++ PZ_02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            Console.Write("Введите значение a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            double n, m, k;
+            if (b > 10.5)
+            { n = 38 * a / (13 * (a - b)); }
+            else
+            { n = 12 * Math.Cos(0.1 * b); }
+            if (n <= 2)
+            { m = Math.Sin(a * Math.Pow(n, 2)); }
+            else
+            { m = 21 * a * n + 2.5 * Math.Pow(n, 2); }
+            k = Math.Pow((a + n), 4) + Math.Pow((b + m), 2);
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"n: {n}");
+            Console.WriteLine($"m: {m}");
+            Console.WriteLine($"k: {k}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PZ_03/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите сумму покупки:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите текущий день недели:  ");
+
+string d = Console.ReadLine();
+
+switch (d)
+{
+    case "Понедельник":
+        int b = Convert.ToInt32(a * 0.97);
+        Console.WriteLine($"Скидка 3%, итого {b}");
+        break;
+    case "Вторник":
+        int q = Convert.ToInt32(a * 0.97);
+        Console.WriteLine($"Скидка 3%, итого {q}");
+        break;
+    case "Среда":
+        int w = Convert.ToInt32(a * 0.97);
+        Console.WriteLine($"Скидка 3%, итого {w}");
+        break;
+    case "Четверг":
+        int e = Convert.ToInt32(a * 0.97);
+        Console.WriteLine($"Скидка 3%, итого {e}");
+        break;
+    case "Пятница":
+        int r = Convert.ToInt32(a * 0.97);
+        Console.WriteLine($"Скидка 3%, итого {r}");
+        break;
+    case "Суббота":
+        int t = Convert.ToInt32(a * 0.93);
+        Console.WriteLine($"Скидка 7%, итого {t}");
+        break;
+    case "Воскресенье":
+        int y = Convert.ToInt32(a * 0.93);
+        Console.WriteLine($"Скидка 7%, итого {y}");
+        break;
+}
