75e8e8535ab40f5f7b1e07fcd6513b95fe28b1f4
Иван Бабан <zxc228deadinside@yandex.ru>
2023-09-22 14:10:47 +0500

    добавлено ПЗ 5

---
+++ PZ_05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальную сумму вклада");
+            double x = Convert.ToDouble(Console.ReadLine()); // начальная сумма вклада 
+            Console.WriteLine("Введите прицентную ставку");
+            double p = Convert.ToDouble(Console.ReadLine()); // годовая процентная ставка 
+            Console.WriteLine("Введите необходимую сумму");
+            double y = Convert.ToDouble(Console.ReadLine()); // необходимая сумма 
+            int z = 0;                                       // количество лет
+
+            while (x < y)
+            {
+                x += x * (p / 100);                          // увеличиваем сумму на p процентов
+                x = Math.Floor(x);                           // отбрасываем дробную часть копеек
+                z++;                                         // увеличиваем счетчик лет
+            }
+
+            Console.WriteLine($"Через {z} лет вклад составит не менее {y} рублей.");
+        }
+    }
+}
