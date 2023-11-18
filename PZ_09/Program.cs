302f4d1aa4cec5a8827a05072759d419585a658d
Иван Бабан <zxc228deadinside@yandex.ru>
2023-11-18 22:51:19 +0500

    Добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");                       // Ввод строки
+            string input = Console.ReadLine();                          //
+
+            int first_spc = input.IndexOf(' ');                         // Определение индексов 
+            int last_spc = input.LastIndexOf(' ');                      // первого и последнего пробелов
+
+            string first_word = input.Substring(0, first_spc);          // Выделение подстрок
+            string last_word = input.Substring(last_spc + 1);           // первого и последнего слов
+
+            string output = last_word + input.Substring(first_spc, last_spc - first_spc) + " " + first_word; // Создание новой строки из обрезков старой
+
+            Console.WriteLine("Результат: " + output);                  // Вывод
+        }
+    }
+}
