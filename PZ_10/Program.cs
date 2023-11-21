e888b158af7d1e574d1eb00b5f504c48b1fa28dd
Иван Бабан <zxc228deadinside@yandex.ru>
2023-11-21 22:39:12 +0500

    ПЗ 10 добавлена

---
+++ PZ_10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();    // ввод текста
+            int middle = (text.Length - 1) / 2;  // определение центра текста
+            if (text.Length % 2 == 0)            // если четное кол-во символов
+                Console.WriteLine($"{text[middle]}{text[middle + 1]}");  // вывод двух центральных символов 
+            else   // иначе (нечетное)
+            {
+                Console.WriteLine(text[middle]);         // вывод единственного центрального символа
+            }
+        }
+    }
+}
