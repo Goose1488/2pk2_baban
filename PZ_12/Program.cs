2091e891152cce65b7682aaf8c976ff25b4a7cd2
Иван Бабан <babaniwan@yandex.ru>
2023-11-28 12:43:35 +0300

    12 практика добавлена

---
+++ PZ_12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PZ_12
+{
+    class Program
+    {
+        static void fio(out string init, string full)      
+        {
+            string[] fullsp = full.Split(' ');
+            string name = fullsp[1];
+            string sname = fullsp[2];
+            init = fullsp[0]+" "+name[0]+"."+sname[0]+".";
+        }
+        static void Main(string[] args)
+        {
+            string result;
+            string full = Console.ReadLine();
+            fio(out result, full);
+            Console.WriteLine(result);
+        }
+    }
+}
