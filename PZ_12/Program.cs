d22e040246d0da3f64e7c2e8d9b9804ac53e5e8e
Иван Бабан <babaniwan@yandex.ru>
2023-11-28 12:49:33 +0300

    12 практика добавлена

--- PZ_12/Program.cs
+++ PZ_12/Program.cs
@@ -4,17 +4,18 @@
     {
         static void fio(out string init, string full)      
         {
-            string[] fullsp = full.Split(' ');
-            string name = fullsp[1];
-            string sname = fullsp[2];
-            init = fullsp[0]+" "+name[0]+"."+sname[0]+".";
+            string[] fullsp = full.Split(' ');  // Строка делится на массив слов
+            string name = fullsp[1]; // Переменная с 1 буквой имени
+            string sname = fullsp[2]; // Переменная с 1 буквой отчества
+            init = fullsp[0]+" "+name[0]+"."+sname[0]+"."; // Вывод в необходимом формате
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ФИО:");
             string result;
             string full = Console.ReadLine();
-            fio(out result, full);
-            Console.WriteLine(result);
+            fio(out result, full);     // Обращение к функции
+            Console.WriteLine(result); // Вывод в консоль
         }
     }
 }
